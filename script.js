0456dd01ed02650683eb50012b4c08e7cd8b7602
Kuldip Singh <159125809+itskuldipsingh@users.noreply.github.com>
2024-07-08 13:08:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const copyCodeBtns = document.querySelectorAll('.copy-code-btn');
-
-copyCodeBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const code = btn.previousSibling.textContent;
-    navigator.clipboard.writeText(code);
-    alert('Code copied to clipboard!');
-  });
-});
